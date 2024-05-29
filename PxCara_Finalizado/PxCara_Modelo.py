e5c15caf9e57572fbc10fe799864f6e49ec67ddb
Ali <ali@prueba.com>
2024-05-29 14:14:20 +0200

    FINAL

--- PxCara_Finalizado/PxCara_Modelo.py
+++ PxCara_Finalizado/PxCara_Modelo.py
@@ -19,11 +19,11 @@ for directory in [INPUT_IMAGES_DIR, PROCESSED_IMAGES_DIR]:
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def detect_faces(image_path):
+def detectar_caras(image_path):
     results = model_face_detection(image_path)
     return results[0].boxes
 
-def read_image(image_path):
+def leer_imagen(image_path):
     img = cv2.imread(image_path)
     if img is None:
         raise FileNotFoundError(f"Imagen en ruta {image_path} no ha sido encontrada / no se ha podido leer.")
@@ -32,7 +32,7 @@ def read_image(image_path):
 def resize_face(face, size=(200, 200)):
     return cv2.resize(face, size) if face.shape[:2] != size else face
 
-def pixelate_face(face, pixel_percentage=0.1):
+def pixelar_cara(face, pixel_percentage=0.1):
     pixel_size = max(1, int(min(face.shape[:2]) * pixel_percentage))
     small = cv2.resize(face, (face.shape[1] // pixel_size, face.shape[0] // pixel_size), interpolation=cv2.INTER_LINEAR)
     return cv2.resize(small, face.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
@@ -52,7 +52,7 @@ def cuadrar_extender_area_cara(img, top_left_x, top_left_y, bottom_right_x, bott
 
     return new_top_left_x, new_top_left_y, new_bottom_right_x, new_bottom_right_y
 
-def draw_prediction_text(face, prediction):
+def dibujar_prediccion_texto(face, prediction):
     text = f"{prediction:.2f}"
     text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.5, 3)[0]
     text_x = (face.shape[1] - text_size[0]) // 2
@@ -60,7 +60,7 @@ def draw_prediction_text(face, prediction):
     cv2.putText(face, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 3)
     return face
 
-def process_faces(img, boxes, age_model, extension_percentage=0.26):
+def procesar_caras(img, boxes, age_model, extension_percentage=0.26): # 26 parece el ideal
     resized_faces = []
     for box in boxes:
         x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -74,14 +74,14 @@ def process_faces(img, boxes, age_model, extension_percentage=0.26):
     for i, (face, (new_top_left_x, new_top_left_y, new_bottom_right_x, new_bottom_right_y)) in enumerate(resized_faces):
         face_resized = np.expand_dims(face, axis=0)
         prediction = age_model.predict(face_resized)[0][0]
-        is_minor = prediction > 0.4
+        is_minor = prediction > 0.38 # -> umbral
 
         if is_minor:
-            face = pixelate_face(face)
+            face = pixelar_cara(face)
         else:
-            cv2.rectangle(face, (0, 0), (199, 199), (0, 255, 0), 2) # que hace esto (el '2' del final)?????????
+            cv2.rectangle(face, (0, 0), (199, 199), (0, 255, 0), 2)
 
-        face = draw_prediction_text(face, prediction)
+        face = dibujar_prediccion_texto(face, prediction)
         resized_faces[i] = (face, (new_top_left_x, new_top_left_y, new_bottom_right_x, new_bottom_right_y))
 
     for i, (face, (new_top_left_x, new_top_left_y, new_bottom_right_x, new_bottom_right_y)) in enumerate(resized_faces):
@@ -91,14 +91,10 @@ def process_faces(img, boxes, age_model, extension_percentage=0.26):
     return img
 
 def process_image_with_face_detection_and_age_classification(image_path, age_model):
-    start_time = time.time()
-    boxes = detect_faces(image_path)
-    img = read_image(image_path)
-    img = process_faces(img, boxes, age_model)
-    end_time = time.time()
-    total_time = end_time - start_time
-    print(f'Tiempo ejecucion: {total_time}') # -> en proceso de desarollo
-    return img, total_time
+    boxes = detectar_caras(image_path)
+    img = leer_imagen(image_path)
+    img = procesar_caras(img, boxes, age_model)
+    return img
 
 @app.route('/process_images', methods=['POST'])
 def process_image():
@@ -109,14 +105,18 @@ def process_image():
         return jsonify({'error': 'La imagen no existe o no se proporciono image_path'}), 400
 
     try:
-        processed_image, total_time = process_image_with_face_detection_and_age_classification(image_path, age_model)
+        comenz_t = time.time()
+        processed_image = process_image_with_face_detection_and_age_classification(image_path, age_model)
         processed_image_name = os.path.basename(image_path).rsplit('.', 1)[0] + '_procesado.jpg'
         processed_image_path = os.path.join(PROCESSED_IMAGES_DIR, processed_image_name)
         cv2.imwrite(processed_image_path, processed_image)
+        fin_t = time.time()
+        total_t = fin_t - comenz_t
+        print(f'Tiempo ejecucion: {total_t}') # -> en proceso de desarollo
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-    return jsonify({'processed_image_path': processed_image_path, 'total_time': total_time}), 200
+    return jsonify({'processed_image_path': processed_image_path}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- api_gateway/static/script.js
+++ api_gateway/static/script.js
@@ -1,94 +1,3 @@
-// const dropZone = document.getElementById('drop-zone');
-// const fileUpload = document.getElementById('fileUpload');
-// let isProcessing = false;
-
-// document.body.addEventListener('dragover', (event) => {
-//     event.preventDefault();
-//     dropZone.classList.add('dragging');
-// });
-
-// document.body.addEventListener('dragleave', (event) => {
-//     if (!event.relatedTarget || !dropZone.contains(event.relatedTarget)) {
-//         dropZone.classList.remove('dragging');
-//     }
-// });
-
-// document.body.addEventListener('drop', (event) => {
-//     event.preventDefault();
-//     dropZone.classList.remove('dragging');
-//     if (dropZone.contains(event.target)) {
-//         const files = event.dataTransfer.files;
-//         handleFiles(files);
-//     }
-// });
-
-// dropZone.addEventListener('dragover', (event) => {
-//     event.preventDefault();
-//     dropZone.classList.add('dragging');
-// });
-
-// dropZone.addEventListener('dragleave', (event) => {
-//     dropZone.classList.remove('dragging');
-// });
-
-// dropZone.addEventListener('drop', (event) => {
-//     event.preventDefault();
-//     dropZone.classList.remove('dragging');
-//     const files = event.dataTransfer.files;
-//     handleFiles(files);
-// });
-
-// fileUpload.addEventListener('change', () => {
-//     const files = fileUpload.files;
-//     handleFiles(files);
-// });
-
-// function handleFiles(files) {
-//     if (isProcessing) return; // limitar a un proceso
-
-//     for (const file of files) {
-//         if (file.type === 'image/jpeg' || file.type === 'image/png') {
-//             isProcessing = true;
-//             uploadAndProcessImage(file);
-//         } else {
-//             alert('Formato no admitido. Por favor, selecciona una imagen JPG o PNG.');
-//         }
-//     }
-// }
-
-// function uploadAndProcessImage(file) {
-//     const formData = new FormData();
-//     formData.append('image', file);
-
-//     fetch('/api/process_images', {
-//         method: 'POST',
-//         body: formData
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         if (data.error) {
-//             alert('Error: ' + data.error);
-//         } else {
-//             const processedImagePath = data.processed_image_path;
-//             const prcesedTotal_time = data.total_time;
-//             openImageInPopup(processedImagePath, prcesedTotal_time);
-//         }
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//         alert('Error al procesar la imagen.');
-//     })
-//     .finally(() => {
-//         isProcessing = false; // dejar uploads nuevos
-//     });
-// }
-
-// function openImageInPopup(imagePath, ttime) {
-//     const popup = window.open('', '_blank', 'width=800,height=600');
-//     popup.document.write(`<h2>Tiempo ejecución: "${ttime}"</h2><br><img src="${imagePath}" alt="Imagen Procesada" style="max-width: 100%; height: auto;">`);
-// }
-
-
 const dropZone = document.getElementById('drop-zone');
 const fileUpload = document.getElementById('fileUpload');
 let isProcessing = false;
@@ -135,7 +44,7 @@ fileUpload.addEventListener('change', () => {
 });
 
 function handleFiles(files) {
-    if (isProcessing) return; // Limitar a un proceso
+    if (isProcessing) return; // limitar a un proceso
 
     for (const file of files) {
         if (file.type === 'image/jpeg' || file.type === 'image/png') {
@@ -170,11 +79,11 @@ function uploadAndProcessImage(file) {
         alert('Error al procesar la imagen.');
     })
     .finally(() => {
-        isProcessing = false; // Dejar subir nuevos
+        isProcessing = false; // dejar subir nuevos
     });
 }
 
 function openImageInPopup(imagePath, totalTime) {
     const popup = window.open('', '_blank', 'width=800,height=600');
-    popup.document.write(`<h2>Tiempo de ejecución: ${totalTime} segundos</h2><br><img src="${imagePath}" alt="Imagen Procesada" style="max-width: 100%; height: auto;">`);
+    popup.document.write(`<img src="${imagePath}" alt="Imagen Procesada" style="max-width: 100%; height: auto;">`);
 }
